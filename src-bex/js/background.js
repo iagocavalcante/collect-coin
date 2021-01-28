521d848c21bc1ca0e890aae353d555f302589664
Iago Cavalcante <iagoangelimc@gmail.com>
2021-01-28 12:51:37 -0300

    feat: limiting domain only to twitch

--- src-bex/js/background.js
+++ src-bex/js/background.js
@@ -5,10 +5,14 @@ chrome.runtime.onMessage.addListener(function (req, sender) {
 })
 
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.alarms.create('COLLECT_COIN', { delayInMinutes: 0.01, periodInMinutes: 0.1 })
+  if (document.domain === 'twitch.com') {
+    chrome.alarms.create('COLLECT_COIN', { delayInMinutes: 0.01, periodInMinutes: 0.1 })
+  }
 })
 
 chrome.runtime.onStartup.addListener(function () {
-  chrome.alarms.create('COLLECT_COIN', { delayInMinutes: 0.01, periodInMinutes: 0.1 })
-  console.log('Starting browser... updating icon.')
+  if (document.domain === 'twitch.com') {
+    chrome.alarms.create('COLLECT_COIN', { delayInMinutes: 0.01, periodInMinutes: 0.1 })
+    window.console.log('Starting browser... updating icon.')
+  }
 })
